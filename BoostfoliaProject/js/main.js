1366680d7313629faa2dcc42424091cae0798c7c
Aleksandr Karadzikov <karadzikovaleksandr@gmail.com>
2019-07-31 09:46:53 +0300

    Проект BoostfoliaProject, сверстан шаблон страницы, осталось добавить адаптив, и настроить оставшийся функционал

---
+++ BoostfoliaProject/js/main.js
@@ -0,0 +1,43 @@
+// плавный скролл к якорю
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function() {
+        let _href = $(this).attr('href');
+        $("html, body").animate({
+                scrollTop: $(_href).offset().top + 'px'
+        },1000);
+    })
+// map settings
+$('.map-block').hide();
+$('.map-heading').on('click', function() {
+    $(this).toggleClass('active');
+    $('.map-block').slideToggle(400);
+})
+})
+
+
+
+
+// Счетчик лайков
+
+let up = document.querySelectorAll('.up'),
+    upRes = document.querySelectorAll('.up-res'),
+    countArr = [0, 0, 0];
+
+
+    if(localStorage.getItem('count')){
+        countArr = JSON.parse(localStorage.getItem('count'));
+        for(let i = 0; i < upRes.length; i++){
+            upRes[i].innerHTML = countArr[i];
+        }
+    }
+
+    up.forEach(function(item, i) {
+        item.addEventListener('click', function() {
+            countArr[i]++;
+            upRes[i].innerHTML = countArr[i];
+            localStorage.setItem('count', JSON.stringify(countArr));
+        });
+    });
+    
+ 
+   
