28317016d8d2f165ae5c99d38484f8a5bfd408a3
Aleksandr Karadzikov <karadzikovaleksandr@gmail.com>
2019-07-31 17:09:54 +0300

    Начало работы на проектом Kids, создана базовая структура

--- BoostfoliaProject/js/main.js
+++ BoostfoliaProject/js/main.js
@@ -49,3 +49,105 @@ window.addEventListener('scroll', function() {
     
     contact.style.backgroundPositionY = (offset - contact.offsetTop) * 0.7  + 'px';
 })
+
+// отправка сообщения пользователем
+
+let msgStatus = {
+    loading: '<img src="img/loading.gif" alt="Загрузка...">',
+    succsess:'Ваше сообщение отправленно, скоро мы с вами свяжемся!',
+    failer:'Что-то пошло не так, сообщение не может быть отправленно'
+};
+
+let form = document.querySelector('.contact-form'),
+    sbmButton = form.querySelector('.contact-btn'),
+    statusMessage = document.createElement('div');
+    statusMessage.classList.add('loading');
+
+    sbmButton.addEventListener('click', function(event) {
+       event.preventDefault();
+       form.appendChild(statusMessage); 
+       statusMessage.innerHTML ="";
+
+        let name = form.name.value,
+            email = form.email.value,
+            message = form.message.value;
+
+
+      
+        let param = 'name=' + name + '&email=' + email + '&message=' + message,
+            url = 'js/message.json';
+
+            if(name === '' || name.length < 3){
+                form.name.style.borderBottom = '1px solid red';
+                
+            }
+            if(email === '' || email.length < 3){
+                form.email.style.borderBottom = '1px solid red';
+            }
+
+            if(message === '' || message.length < 10){
+                form.message.style.borderBottom = '1px solid red';
+            }else{
+                setMessage(url, function(data) {
+                    form.name.style.borderBottom = '1px solid green';
+                    form.email.style.borderBottom = '1px solid green';
+                    form.message.style.borderBottom = '1px solid green';
+                    succsess(data);
+                });
+            }
+
+
+           
+
+    });
+
+
+    function setMessage(url, callback) {
+
+        // let func = callback || function(data){},
+           let  request = new XMLHttpRequest();
+
+            request.open('GET', url);
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            request.send();
+
+            
+            request.onreadystatechange = function() {
+                if(request.readyState != 4) {
+                        return false;
+                }
+
+                
+                sbmButton.disabled = false;
+                sbmButton.innerHTML = 'SEND MESSAGE';
+
+                if(request.status !=200){
+                    console.log(request.status + ':' + request.statusText);
+                    statusMessage.innerHTML = msgStatus.failer;
+                }else{
+                        // конвентирую полученный ответ с сервера в виде JSON в обычный обьект
+                        let data = JSON.parse(request.responseText);
+                    // функиция callback с полученным ответом 
+                    callback(data);
+                }
+
+            };
+
+            statusMessage.innerHTML = msgStatus.loading;
+            sbmButton.innerHTML = 'Отправка..';
+            sbmButton.disabled = true;
+    };
+
+
+    
+ function succsess(data) {
+     form.reset();
+     statusMessage.innerHTML = data.success;
+     
+     setTimeout(function() {
+        statusMessage.parentElement.removeChild(statusMessage);
+        form.name.style.borderBottom = '1px solid #e9dddd';
+        form.email.style.borderBottom = '1px solid #e9dddd';
+        form.message.style.borderBottom = '1px solid #e9dddd';
+     }, 3000)
+ }
