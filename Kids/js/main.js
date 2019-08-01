a6fb0277398eae60265ea55723fe6020e7ef481c
Aleksandr Karadzikov <karadzikovaleksandr@gmail.com>
2019-08-01 18:54:56 +0300

    Исправлен логотип, а так же создан блок main

--- Kids/js/main.js
+++ Kids/js/main.js
@@ -5,3 +5,12 @@ $(document).ready(function(){
         $('.header-mobile-navigation').slideToggle(400);
     });
 });
+
+// slick init
+// main slick slider
+$(document).ready(function(){
+    $('.main-slider').slick({
+        nextArrow:'<button type="button" class="arrow arrow-right"><i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></button>',
+        prevArrow:'<button type="button" class="arrow arrow-left"><i class="fa fa-arrow-circle-o-left" aria-hidden="true"></i></button>'
+    });
+});
