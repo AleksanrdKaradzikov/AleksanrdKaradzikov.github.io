f4ac9e4c57142e8353fb17659294eaefd4f2c535
Aleksandr Karadzikov <karadzikovaleksandr@gmail.com>
2019-08-08 18:18:12 +0300

    Создана страница услуги

--- Kids/js/main.js
+++ Kids/js/main.js
@@ -27,6 +27,12 @@ $(document).ready(function(){
         nextArrow:'<button type="button" class="arrow arrow-right"><i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></button>',
         prevArrow:'<button type="button" class="arrow arrow-left"><i class="fa fa-arrow-circle-o-left" aria-hidden="true"></i></button>'
     });
+    
+    $('.services-block .img-block').slick({
+        nextArrow:'<button type="button" class="arrow arrow-right">&rsaquo;</button>',
+        prevArrow:'<button type="button" class="arrow arrow-left">&lsaquo;</button>'
+    });
+
 });
 
 // модальные окна
